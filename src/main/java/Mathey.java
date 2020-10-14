a250801682d173ddac8265169cdb19073a955e6c
22cburrows <22cburrows@athenian.org>
2020-10-14 20:46:00 +0000

    Practice with overloading

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,23 @@
 public class Mathey {
-
+    public static int rand() {
+        return (int)(Math.random()*20.0) + 1;
+    }
+    public static int max(double a1, double b1) {
+        return Math.max(a, b);
+    }
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+    public static int max(int a, int b, int c, int d) {
+        return Math.max(a, Math.max(b, Math.max(c, d)));
+    }
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+    public static int min(double a1, double b1) {
+        return (double) Math.min(a, b);
+    }
+    public static int min(int a, int b, int c, int d) {
+        return Math.min(a, Math.min(b, Math.min(c, d)));
+    }
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    public static int someMethod() {
-        return 1;
+    public static int rand() {
+        return (int) (Math.random()*20) + 1;
+    }
+    public static int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+    public static int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = rand();
+        int b = rand();
+        int c = rand();
+        System.out.println("The three random numbers are: " + a + ", " + b + ", " + c);
+        System.out.println("The largest number is: " + max(a, b, c));
+        System.out.println("The smallest number is: " + min(a, b, c));
     }
 
 }
